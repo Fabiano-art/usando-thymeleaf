c1e35fb667ef6ed2e0d45f687e6450de579ea8c6
Fabiano Guimarães <fabio.dub@outlook.com>
2021-03-10 15:55:42 -0300

    pagina de posts e metodo get para post especifico

--- src/main/java/com/blog/controller/PostController.java
+++ src/main/java/com/blog/controller/PostController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.blog.model.Post;
@@ -24,4 +25,11 @@ public class PostController {
 		return mv;
 	}
 	
+	@GetMapping("/posts/{id}")
+	public ModelAndView getPostDetails(@PathVariable("id") Long id) {
+		ModelAndView mv = new ModelAndView("postDetails");
+		Post post = ser.findById(id);
+		mv.addObject("post", post);
+		return mv;
+	}
 }
